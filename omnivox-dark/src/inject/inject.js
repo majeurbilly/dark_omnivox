3c298f52305b2ea50e333424f099744c1a008825
majeurbilly <billy.halle.bh@gmail.com>
2025-01-20 22:26:20 -0500

    upadete inject.js

--- omnivox-dark/src/inject/inject.js
+++ omnivox-dark/src/inject/inject.js
@@ -1,12 +1,3 @@
-
-
-
-
-
-
-function activerDark() {  // Fonction pour activer le mode sombre
-    document.getElementsByTagName("html")[0].classList.remove("vanilla")
-}
 var div = document.createElement('a');// création d'un élément <a> dynamiquement
 div.setAttribute('id', 'themeToggle')// ID à l'élément pour le manipuler plus facilement
 div.setAttribute('title', 'Toggle Light/Dark Mode')// titre pour indiquer la fonctionnalité du bouton
@@ -15,31 +6,20 @@ div.setAttribute('style', 'display: flex; height: 100%; justify-content: center;
 div.innerHTML = `<img style="height: 60%" src="${chrome.runtime.getURL('/src/inject/icon.svg')}" class="logo-lea" alt="Toggle Light/Dark Mode">`;// élément bouton au DOM, en tant qu'enfant d'un élément existant
 document.getElementById('wrapper-headerOmnivoxLogo').appendChild(div);// événements sur le bouton pour détecter les clics
 
-document.getElementById("themeToggle").addEventListener("click", () => {// événements sur le bouton pour détecter les clics
-    if (darkModeActive === 'yes') {    // vérifie si le mode sombre est activé
-
-        desactiverDark();// Désactive le mode sombre
-        chrome.storage.sync.set({dark_mode: 'no'}, function () {
-            darkModeActive = 'no'
-        });
-    } else if (darkModeActive === 'no') { // Vérifier le navigateur du user pour voir si il est sur dark ou light et set le mode
-        activerDark()
-        chrome.storage.sync.set({dark_mode: 'yes'}, function () {
-            darkModeActive = 'yes'
-        });
-    }
-});
 
 window.onload = async function () {// exécuté au chargement de la page
 
     var darkModeActive;  // Variable pour stocker l'état du mode sombre
 
-
+    function activerDark() {  // Fonction pour activer le mode sombre
+        document.getElementsByTagName("html")[0].classList.remove("vanilla")
+    }
 
     function desactiverDark() {  // Fonction pour désactiver le mode sombre
         console.log('disabling')
         document.getElementsByTagName("html")[0].classList.add("vanilla")
     }
+
     // Récupère la valeur actuelle du mode sombre dans le stockage de Chrome
     await chrome.storage.sync.get(['dark_mode'], function (result) {
         darkModeActive = result['dark_mode']
@@ -54,7 +34,20 @@ window.onload = async function () {// exécuté au chargement de la page
             });
         }
 
+        document.getElementById("themeToggle").addEventListener("click", () => {// événements sur le bouton pour détecter les clics
+            if (darkModeActive === 'yes') {    // vérifie si le mode sombre est activé
 
+                desactiverDark();// Désactive le mode sombre
+                chrome.storage.sync.set({dark_mode: 'no'}, function () {
+                    darkModeActive = 'no'
+                });
+            } else if (darkModeActive === 'no') { // Vérifier le navigateur du user pour voir si il est sur dark ou light et set le mode
+                activerDark()
+                chrome.storage.sync.set({dark_mode: 'yes'}, function () {
+                    darkModeActive = 'yes'
+                });
+            }
+        });
     });
 
 
